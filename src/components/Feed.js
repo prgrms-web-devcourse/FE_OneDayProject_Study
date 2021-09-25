f39af61b189b5516d34028e8e4d252346487e94f
grighth12 <grighth12@gmail.com>
2021-09-25 16:10:49 +0900

    fix: 글 클릭 시 새 창으로 열리게 함

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -43,5 +43,13 @@ export default function Feed({ $target, initialState }) {
     `
   }
 
+  $feed.addEventListener('click', (e) => {
+    e.preventDefault()
+    const { className } = e.target
+    if (className === 'feed__link') {
+      window.open(e.target.href)
+    }
+  })
+
   this.render()
 }
